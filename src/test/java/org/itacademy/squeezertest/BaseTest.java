5e3f1527a531590aa381f58f8dacf0c2c5821d3e
Konstantin <novash.ki@gmail.com>
2023-07-24 15:10:13 +0300

    Parameters was added

--- src/test/java/org/itacademy/squeezertest/BaseTest.java
+++ src/test/java/org/itacademy/squeezertest/BaseTest.java
@@ -11,6 +11,7 @@ import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import java.net.MalformedURLException;
@@ -25,34 +26,30 @@ public class BaseTest {
     private final ResourceBundle bundle = ResourceBundle.getBundle("config");
     public final String SERVERADDRESS = bundle.getString("serverAddress");
 
-    public final String appPackage = bundle.getString("appPackage");
-    public final String appActivity = bundle.getString("appActivity");
-    public final String platformName = bundle.getString("platformName");
-    public final String platformVersion = bundle.getString("platformVersion");
-    public final String deviceName = bundle.getString("deviceName");
-    public final String appiumDriver = bundle.getString("appiumDriver");
+//    public final String platformName = bundle.getString("platformName");
+//    public final String platformVersion = bundle.getString("platformVersion");
+//    public final String deviceName = bundle.getString("deviceName");
+//    public final String appiumDriver = bundle.getString("appiumDriver");
+//    public final String appPackage = bundle.getString("appPackage");
+//    public final String appActivity = bundle.getString("appActivity");
 
     private AppiumDriver<MobileElement> driver;
 
     @BeforeClass
-//    @Parameters({"platformName", "platformVersion", "deviceName", "appiumDriver", "appPackage", "appActivity"})
+    @Parameters({"platformName", "platformVersion", "deviceName", "appiumDriver", "appPackage", "appActivity"})
     public void beforeMethod(
-//            String platformName,
-//            String platformVersion,
-//            String deviceName,
-//            String appiumDriver,
-//            String appPackage,
-//            String appActivity
+            @Optional("platformName") String platformName,
+            @Optional("platformVersion") String platformVersion,
+            @Optional("deviceName") String deviceName,
+            @Optional("appiumDriver") String appiumDriver,
+            @Optional("appPackage") String appPackage,
+            @Optional("appActivity") String appActivity
     ) {
         log.info("BEFORE METHOD");
 
         log.info("START TYPE LOCAL");
         SelenideLogger.addListener("allure", new AllureSelenide()
                 .screenshots(true));
-//                .savePageSource(true)
-//                .enableLogs(LogType.BROWSER, Level.ALL));
-
-
         WebDriverRunner.closeWebDriver();
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("appPackage", appPackage);
